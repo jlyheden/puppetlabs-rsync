486f2341c3707d0c7bf239bde01dee447e010295
jlyheden <johan@lyheden.com>
2013-07-24 16:23:14 +0200

    support for exclude-file, debug-mode and exec will now trigger on failures so that errors does not silently happen

--- manifests/get.pp
+++ manifests/get.pp
@@ -10,6 +10,9 @@
 #   $exlude  - string to be excluded
 #   $keyfile - path to ssh key used to connect to remote host, defaults to /home/${user}/.ssh/id_rsa
 #   $timeout - timeout in seconds, defaults to 900
+#   $debug   - if exec should send notice, boolean
+#   $excludes - array of multiple exclude pattern, will create a file for it
+#   $cron_run_interval - cron type param minute
 #
 # Actions:
 #   get files via rsync
@@ -31,7 +34,10 @@ define rsync::get (
   $purge = undef,
   $exclude = undef,
   $keyfile = undef,
-  $timeout = '900'
+  $timeout = '900',
+  $debug = false,
+  $excludes = undef,
+  $cron_run_interval = undef
 ) {
 
   if $keyfile {
@@ -58,18 +64,47 @@ define rsync::get (
     $MyPath = $name
   }
 
-  $rsync_options = "-a ${MyPurge} ${MyExclude} ${MyUser}${source} ${MyPath}"
+  if $excludes {
+    $exclude_sanitized_file_name = regsubst($name,'[^\w\d]','_','G')
+    $exclude_file_path = "/tmp/${exclude_sanitized_file_name}_excludes"
+    $MyExcludes = "--exclude-from=${exclude_file_path}"
+    file { $exclude_file_path:
+      ensure => file,
+      content => inline_template("This file is being maintained by Puppet.\n# DO NOT EDIT\n<%= @excludes.join('\n') %>")
+    }
+    if $cron_run_interval == undef {
+      File[$exclude_file_path] -> Exec["rsync ${name}"]
+    }
+  }
 
-  exec { "rsync ${name}":
-    command => "rsync -q ${rsync_options}",
-    path    => [ '/bin', '/usr/bin' ],
-    # perform a dry-run to determine if anything needs to be updated
-    # this ensures that we only actually create a Puppet event if something needs to
-    # be updated
-    # TODO - it may make senes to do an actual run here (instead of a dry run)
-    #        and relace the command with an echo statement or something to ensure
-    #        that we only actually run rsync once
-    onlyif  => "test `rsync --dry-run --itemize-changes ${rsync_options} | wc -l` -gt 0",
-    timeout => $timeout,
+  $rsync_options = "-a ${MyPurge} ${MyExclude} ${MyExcludes} ${MyUser}${source} ${MyPath}"
+  $exec_command = "rsync -q ${rsync_options}"
+  $exec_onlyif = "test `rsync --dry-run --itemize-changes ${rsync_options} 2>&1 | wc -l` -gt 0"
+
+  if $cron_run_interval {
+    cron { "rsync ${name}":
+      command => "/usr/bin/env ${exec_command}",
+      minute  => $cron_run_interval
+    }
+  } else {
+    exec { "rsync ${name}":
+      command => $exec_command,
+      path    => [ '/bin', '/usr/bin' ],
+      # perform a dry-run to determine if anything needs to be updated
+      # this ensures that we only actually create a Puppet event if something needs to
+      # be updated
+      # TODO - it may make senes to do an actual run here (instead of a dry run)
+      #        and relace the command with an echo statement or something to ensure
+      #        that we only actually run rsync once
+      # NOTE - Pipe stderr to stdout so that exec will actually trigger on errors
+      #        so that we can catch it
+      onlyif  => $exec_onlyif,
+      timeout => $timeout,
+    }
+  }
+
+  if $debug {
+    notice("Rsync command: ${exec_command}")
+    notice("Rsync onlyif: ${exec_onlyif}")
   }
 }
